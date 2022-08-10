52bb7693549a6e2e2dba92334a2fdcfdef824b6f
ParsaKamyar <atthar4683612@icloud.com>
2022-08-10 23:34:09 +0430

    add css, sass, postcss, browserlist

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let mode = 'development';
+// let target = "web"
 
 if (process.env.NODE_ENV === 'production') {
   mode = 'production';
+  // target = "browserslist";
 }
 
 module.exports = {
   mode: mode,
+  // target: target;
+
   module: {
     rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -19,11 +33,16 @@ module.exports = {
       }
     ]
   },
+  plugins: [new MiniCssExtractPlugin()],
 
   devtool: 'source-map',
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist')
-    }
+    },
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true
   }
 };
